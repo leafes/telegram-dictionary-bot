5d72a5bab3f1e1bcc6ca71d5ba9c099f3b158454
leafes <xmm.geek@gmail.com>
2023-11-10 23:00:11 +0900

    change ui lang

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const YANDEX_API_KEY = 'dict.1.1.20231110T132713Z.6452285b573c8808.e7486e46d8f88b228482009985ace433c7e11dcc';
 const API_URL = 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup';
-const QUERIES = `?key=${YANDEX_API_KEY}&lang=ru-ru&text=`;
+const QUERIES = `?key=${YANDEX_API_KEY}&lang=ru-ru&ui=ru&text=`;
 const getMeaning = async (word) => {
   console.log('Выполняю поиск по слову: '+ word);
   const f = await fetch(`${API_URL}${QUERIES}${word}`);
