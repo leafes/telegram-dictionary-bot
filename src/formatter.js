acf16c22fdb86edab2e5099aa862f97123eaea89
leafes <xmm.geek@gmail.com>
2023-11-10 23:26:09 +0900

    minor fixes

--- src/formatter.js
+++ src/formatter.js
@@ -3,8 +3,7 @@ import getMeaning from "./api.js";
 const formatMeaning = async (meaningData) => {
   if (!meaningData) return 'Произошла ошибка!'
   const listOfMeanings = meaningData.tr.map((meaning) => meaning.text)
-  const message = `Поиск по слову: ${meaningData.text}\n----\nЧасть речи:
-   ${meaningData.pos} \nЗначения: ${listOfMeanings.join(', ')}`;
+  const message = `Поиск по слову: ${meaningData.text}\n----\nЧасть речи: ${meaningData.pos} \nЗначения: ${listOfMeanings.join(', ')}`;
   return message;
 }
 
--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('<b>Привет, я бот-словарь, отправь мне слово и я найду его значение в толковом словаре</b>'));
 
 bot.on(message('text'), async (ctx) => {
-  ctx.reply(`Ищу в словаре слово ${ctx.message.text}`)
+  ctx.reply(`🧐 Ищу в словаре слово ${ctx.message.text}`)
   const wordMeanings = await getMeaning(ctx.message.text);
   const formattedMeaning = await formatMeaning(wordMeanings);
-  
+
   console.log(formattedMeaning);
   ctx.reply(formattedMeaning);
   });
